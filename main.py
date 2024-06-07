a5413a355f0cfe5b45e812822164a709348e19a4
Sarvar <sabdumutolibov@gmail.com>
2024-06-07 16:42:46 +0500

    15 ta task

---
+++ main.py
@@ -0,0 +1,18 @@
+
+
+# alf = ["c","b","a","f","g","d","e"]
+# a = sorted(alf)
+# b = enumerate(a)
+# print(dict(b))
+
+
+num = int(input("Son: "))
+result = 0
+x = 0
+while result <= num:
+    x += result
+    result += 1
+
+print(x
+      )
+
